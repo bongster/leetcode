051ab91cb504ec443acc0a15466fae52dfdb9eba
Daniel Bong <bongster88@gmail.com>
2019-07-29 08:57:41 +0700

    update at 2019-07-29

---
+++ solutions/1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -0,0 +1,53 @@
+# The Tribonacci sequence Tn is defined as follows: 
+#
+# T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+#
+# Given n, return the value of Tn.
+#
+#  
+# Example 1:
+#
+#
+# Input: n = 4
+# Output: 4
+# Explanation:
+# T_3 = 0 + 1 + 1 = 2
+# T_4 = 1 + 1 + 2 = 4
+#
+#
+# Example 2:
+#
+#
+# Input: n = 25
+# Output: 1389537
+#
+#
+#  
+# Constraints:
+#
+#
+# 	0 <= n <= 37
+# 	The answer is guaranteed to fit within a 32-bit integer, ie. answer <= 2^31 - 1.
+#
+#
+
+
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        elif n == 2:
+            return 1
+        elif n == 3:
+            return 2
+        
+        n_arr = [0] * (n + 1)
+        n_arr[0] = 0
+        n_arr[1] = 1
+        n_arr[2] = 1
+        for i in range(3, n + 1):
+            n_arr[i] = n_arr[i-1] + n_arr[i - 2] + n_arr[i - 3]
+        
+        return n_arr[n]
